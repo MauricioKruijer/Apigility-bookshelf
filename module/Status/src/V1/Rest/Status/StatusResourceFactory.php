9c2ca3852111e0d1b169baa39fc5871550383d7e
Mauricio Kruijer <mauricio.kruijer@gmail.com>
2019-07-15 23:57:20 +0200

    Add mapper to resource by using factory
    Look up: Zend Framework Service Manager

--- module/Status/src/V1/Rest/Status/StatusResourceFactory.php
+++ module/Status/src/V1/Rest/Status/StatusResourceFactory.php
@@ -1,10 +1,12 @@
 <?php
 namespace Status\V1\Rest\Status;
 
+use StatusLib\MapperInterface;
+
 class StatusResourceFactory
 {
     public function __invoke($services)
     {
-        return new StatusResource();
+        return new StatusResource($services->get(MapperInterface::class));
     }
 }

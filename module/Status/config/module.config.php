73dfbfb6928b3405a8c8a4e186504de21ae2539f
Mauricio Kruijer <mauricio.kruijer@gmail.com>
2019-07-15 23:13:18 +0200

    Added “Status” REST service (boilerplate)

--- module/Status/config/module.config.php
+++ module/Status/config/module.config.php
@@ -17,11 +17,21 @@ return [
                     ],
                 ],
             ],
+            'status.rest.status' => [
+                'type' => 'Segment',
+                'options' => [
+                    'route' => '/status[/:status_id]',
+                    'defaults' => [
+                        'controller' => 'Status\\V1\\Rest\\Status\\Controller',
+                    ],
+                ],
+            ],
         ],
     ],
     'zf-versioning' => [
         'uri' => [
             0 => 'status.rpc.ping',
+            1 => 'status.rest.status',
         ],
     ],
     'zf-rpc' => [
@@ -36,6 +46,7 @@ return [
     'zf-content-negotiation' => [
         'controllers' => [
             'Status\\V1\\Rpc\\Ping\\Controller' => 'Json',
+            'Status\\V1\\Rest\\Status\\Controller' => 'HalJson',
         ],
         'accept_whitelist' => [
             'Status\\V1\\Rpc\\Ping\\Controller' => [
@@ -43,9 +54,18 @@ return [
                 1 => 'application/json',
                 2 => 'application/*+json',
             ],
+            'Status\\V1\\Rest\\Status\\Controller' => [
+                0 => 'application/vnd.status.v1+json',
+                1 => 'application/hal+json',
+                2 => 'application/json',
+            ],
         ],
         'content_type_whitelist' => [
             'Status\\V1\\Rpc\\Ping\\Controller' => [
+                0 => 'application/vnd.status.v1+json',
+                1 => 'application/json',
+            ],
+            'Status\\V1\\Rest\\Status\\Controller' => [
                 0 => 'application/vnd.status.v1+json',
                 1 => 'application/json',
             ],
@@ -64,6 +84,51 @@ return [
                 'filters' => [],
                 'name' => 'ack',
                 'description' => 'Acknowledge the request with a timestamp',
+            ],
+        ],
+    ],
+    'service_manager' => [
+        'factories' => [
+            \Status\V1\Rest\Status\StatusResource::class => \Status\V1\Rest\Status\StatusResourceFactory::class,
+        ],
+    ],
+    'zf-rest' => [
+        'Status\\V1\\Rest\\Status\\Controller' => [
+            'listener' => \Status\V1\Rest\Status\StatusResource::class,
+            'route_name' => 'status.rest.status',
+            'route_identifier_name' => 'status_id',
+            'collection_name' => 'status',
+            'entity_http_methods' => [
+                0 => 'GET',
+                1 => 'PATCH',
+                2 => 'PUT',
+                3 => 'DELETE',
+            ],
+            'collection_http_methods' => [
+                0 => 'GET',
+                1 => 'POST',
+            ],
+            'collection_query_whitelist' => [],
+            'page_size' => 25,
+            'page_size_param' => null,
+            'entity_class' => \Status\V1\Rest\Status\StatusEntity::class,
+            'collection_class' => \Status\V1\Rest\Status\StatusCollection::class,
+            'service_name' => 'Status',
+        ],
+    ],
+    'zf-hal' => [
+        'metadata_map' => [
+            \Status\V1\Rest\Status\StatusEntity::class => [
+                'entity_identifier_name' => 'id',
+                'route_name' => 'status.rest.status',
+                'route_identifier_name' => 'status_id',
+                'hydrator' => \Zend\Hydrator\ArraySerializable::class,
+            ],
+            \Status\V1\Rest\Status\StatusCollection::class => [
+                'entity_identifier_name' => 'id',
+                'route_name' => 'status.rest.status',
+                'route_identifier_name' => 'status_id',
+                'is_collection' => true,
             ],
         ],
     ],
